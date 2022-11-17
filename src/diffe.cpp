dd8b2251a1c445bb32f453f2f2b06c8fa1d4479b
danithered <danithered@live.com>
2022-11-17 13:50:47 +0100

    setClimate
    	modified:   src/diffe.cpp

--- src/diffe.cpp
+++ src/diffe.cpp
@@ -86,7 +86,16 @@ public:
 			}
 	}
 
-	void setClimate(double mean_Tshift, double mean_Tr, double sd_Tshift, double sd_Tr, double length){
+	void setClimate(double mean_Tshift, double mean_Tr, double sd_Tshift=0, double sd_Tr=0, double length=0, unsigned int no_intervals = 1){
+		if(no_intervals > 1){
+			double until = length;
+			while(no_intervals--) {
+				Tpars.emplace(until, TempParams(gsl_ran_gaussian(r, sd_Tshift) + mean_Tshift, gsl_ran_gaussian(r, sd_Tr) + mean_Tr));
+				until += length;
+			}
+		} else { // only one value
+			Tpars.emplace(0, TempParams(mean_Tshift, mean_Tr) );
+		}
 	}
 
 	void setExtreme(unsigned int no, double until){
